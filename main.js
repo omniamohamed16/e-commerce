6d8b66d6b7d633b9908559a79b2df702cc786c5f
omniamohamed16 <omnia.mohamedd.40@gmail.com>
2025-03-22 07:25:49 +0200

    adding sidenav

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("mySidenav").style.width = "300px";
   }
   
   /* Set the width of the side navigation to 0 */
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
+
+  window.addEventListener("click" , function(e){
+       e.stopPropagation()     
+
+    if(e.target.className !== "l1" && e.target.className !== "three-lines" && e.target.className !== "sidenav" ){
+
+        console.log("kk")
+    document.getElementById("mySidenav").style.width = "0";}
+  })
