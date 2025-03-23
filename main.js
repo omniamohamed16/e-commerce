729450da82c46215834f831aabe4d8c115b75caf
omniamohamed16 <omnia.mohamedd.40@gmail.com>
2025-03-23 21:10:33 +0200

    editing navbars

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 function openNav() {
+
     document.getElementById("mySidenav").style.width = "300px";
   }
   
-  /* Set the width of the side navigation to 0 */
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
@@ -10,8 +10,7 @@ function openNav() {
   window.addEventListener("click" , function(e){
        e.stopPropagation()     
 
-    if(e.target.className !== "l1" && e.target.className !== "three-lines" && e.target.className !== "sidenav" ){
+    if(e.target.className !== "l1" && e.target.className !== "three-lines" && e.target.className !== "menu-content" ){
 
-        console.log("kk")
     document.getElementById("mySidenav").style.width = "0";}
   })
