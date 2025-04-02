0e6e16e92b8345f630b05b706bc6cdc16e69480c
omniamohamed16 <omnia.mohamedd.40@gmail.com>
2025-04-02 22:12:21 +0200

    editing collapsables

--- main.js
+++ main.js
@@ -1,30 +1,97 @@
+// opening and closing side nav//
 function openNav() {
+  document.getElementById("mySidenav").style.width = "300px";
+}
 
-    document.getElementById("mySidenav").style.width = "300px";
-  }
-  
-  function closeNav() {
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+window.addEventListener("click", function (e) {
+  e.stopPropagation();
+
+  if (
+    e.target.className !== "l1" &&
+    e.target.className !== "three-lines" &&
+    e.target.className !== "Mills-title" &&
+    e.target.className !== "menu-content"
+  ) {
     document.getElementById("mySidenav").style.width = "0";
   }
+});
 
-  window.addEventListener("click" , function(e){
-    e.stopPropagation()
+menuLinks = document.querySelectorAll(".vertical-menu a");
 
-    if(e.target.className !== "l1" && e.target.className !== "three-lines" && e.target.className !== "Mills-title"  && e.target.className !== "menu-content" ){
+menudiv = document.querySelectorAll(".menu-content div");
 
-    document.getElementById("mySidenav").style.width = "0";}
-  })
+menudiv.forEach((element) => {
+  element.onclick = function (e) {
+    e.stopPropagation();
+  };
+});
 
-  menuLinks = document.querySelectorAll(".vertical-menu a")
+menuLinks.forEach((element) => {
+  element.onclick = function (e) {
+    e.stopPropagation();
+  };
+});
 
-  menudiv = document.querySelectorAll(".menu-content div")
+// hover over links in side nav
 
-  menudiv.forEach(element => {
-    element.onclick= function(e){e.stopPropagation()}
-  });
-  
-  menuLinks.forEach(element => {
-    
-    element.onclick= function(e){e.stopPropagation()}
-  });
-  
+womenSection = document.querySelector(".collapse-women-section");
+womenContent = document.querySelector(".women-content");
+
+womenSection.onmouseover = function (e) {
+  e.stopPropagation();
+};
+womenContent.onmouseover = function (e) {
+  e.stopPropagation();
+};
+
+womenSection.addEventListener("mouseover", (e) => {
+  if (
+    (e.target.className == "women-content",
+    e.target.className == "collapse-women-section")
+  ) {
+    womenContent.style.display = "block";
+    menContent.style.display = "none";
+
+  } else {
+    womenContent.style.display = "none";
+  }
+});
+
+window.addEventListener("mouseover", (e) => {
+  if (e.target.className !== "women-content") {
+    womenContent.style.display = "none";
+  }
+});
+
+
+menSection = document.querySelector(".collapse-men-section");
+menContent = document.querySelector(".men-content");
+
+menSection.addEventListener("mouseover", (e) => {
+  if (
+    (e.target.className == "men-content",
+    e.target.className == "collapse-men-section")
+  ) {
+    menContent.style.display = "block";
+    womenContent.style.display = "none";
+  } else {
+    menContent.style.display = "none";
+  }
+});
+
+menSection.onmouseover = function (e) {
+  e.stopPropagation();
+};
+menContent.onmouseover = function (e) {
+  e.stopPropagation();
+};
+
+window.addEventListener("mouseover", (e) => {
+  if (e.target.className !== "men-content") {
+    menContent.style.display = "none";
+  }
+});
