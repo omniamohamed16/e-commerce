ae4b4b82b156f9d196aa4998984e1c11ca501733
omniamohamed16 <omnia.mohamedd.40@gmail.com>
2025-04-18 21:22:36 +0200

    adding best seller section

--- main.js
+++ main.js
@@ -95,3 +95,24 @@ window.addEventListener("mouseover", (e) => {
     menContent.style.display = "none";
   }
 });
+
+
+
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.style.transform = `translateX(${(i - index) * 100}%)`;
+    });
+}
+
+document.querySelector('.next').addEventListener('click', () => {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+});
+
+document.querySelector('.prev').addEventListener('click', () => {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide(currentSlide);
+});
